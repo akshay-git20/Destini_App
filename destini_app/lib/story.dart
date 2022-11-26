d2e0fc4ed06af0038c003a02992e0881a058d042
akshay <abpotkhule_b21@it.vjti.ac.in>
2022-11-26 15:17:22 +0530

    intialize app

---
+++ destini_app/lib/story.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
+
+class Story {
+  final String storyTitle;
+  final String choice1;
+  final String choice2;
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+}
+
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
